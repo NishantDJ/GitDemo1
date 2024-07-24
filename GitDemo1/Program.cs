df511ade7ef3d12c065b9962a3f5829f89cd71cf
Nishant Jarare <HP@DESKTOP-UB2LPT1>
2024-07-24 20:13:26 +0530

    Add project files.

---
+++ GitDemo1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Main");
+        }
+    }
+}
